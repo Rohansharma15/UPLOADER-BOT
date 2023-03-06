0a09ec74e94747170f868321a02eb0fd0ca07af3
Rohansharma15 <125382159+Rohansharma15@users.noreply.github.com>
2023-03-06 21:59:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5829460112:AAGWaONbfNO9RIdtAY9b6wyXHdH3xDIt6Zs")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", 24512494))
+    API_HASH = os.environ.get("965ff313790ad8d1971a8209c8dfcf85")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    DOWNLOAD_LOCATION = "Youtube-Downloader-Bot"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 4194304000 #2097152000
@@ -24,7 +24,7 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5193168915"))
     SESSION_NAME = "UPLOADER-X-BOT"
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
